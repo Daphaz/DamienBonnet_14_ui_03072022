fd8a78f734ec175a175830f63e4c449d472c147e
Damien Bonnet <bonnet.damien@ymail.com>
2022-07-04 20:16:52 +0200

    feat: button component

--- lib/components/fields/Button/Button.tsx
+++ lib/components/fields/Button/Button.tsx
@@ -1,7 +1,10 @@
 import clsx from 'clsx';
-import React from 'react';
+import React, { useState } from 'react';
 
 import s from './styles.module.scss';
+
+import { Spinner } from '../../common';
+import { ButtonVariant } from './enums';
 
 export interface IButtonProps
   extends React.DetailedHTMLProps<
@@ -9,13 +12,48 @@ export interface IButtonProps
     HTMLButtonElement
   > {
   label: string;
+  shadow?: boolean;
+  variant?: ButtonVariant;
+  borderer?: boolean;
+  loading?: boolean;
 }
 
-const Button = ({ label, className, ...rest }: IButtonProps) => {
-  const classes = clsx(s.btn, s.primary, className);
+const Button = ({
+  label,
+  shadow = true,
+  variant = ButtonVariant.Primary,
+  borderer = false,
+  loading = false,
+  className,
+  ...rest
+}: IButtonProps) => {
+  const [state, setState] = useState<'ready' | 'pressed'>('ready');
+  const classes = clsx(
+    s.btn,
+    s[variant],
+    {
+      [s.isPressed]: state == 'pressed',
+      [s.bs]: shadow,
+      [s.border]: borderer,
+      [s.loading]: loading,
+    },
+    className
+  );
+
   return (
-    <button {...rest} className={classes}>
-      {label}
+    <button
+      {...rest}
+      className={classes}
+      onMouseDown={() => setState('pressed')}
+      onMouseUp={() => setState('ready')}
+      onMouseLeave={() => setState('ready')}
+    >
+      {loading && (
+        <span className={s.spinner}>
+          <Spinner width={18} height={18} />
+        </span>
+      )}
+      <span className={s.text}>{label}</span>
     </button>
   );
 };
--- lib/components/fields/Button/index.ts
+++ lib/components/fields/Button/index.ts
@@ -1,1 +1,2 @@
 export { default as Button, IButtonProps } from './Button';
+export * from './enums';
