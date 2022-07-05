2dc5785d843fca00c309aca5ea2f35d70ec6249f
Damien Bonnet <bonnet.damien@ymail.com>
2022-07-05 19:51:08 +0200

    feat: input component

---
+++ lib/components/fields/Input/Input.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import React from 'react';
+import s from './styles.module.scss';
+
+export interface IInputProps {
+  inputProps: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >;
+  error?: string;
+  classContainer?: string;
+}
+
+const Input = ({ inputProps, classContainer, error }: IInputProps) => {
+  /* istanbul ignore next */
+  const { className, disabled, onChange, ...rest } = inputProps || {};
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (disabled) return;
+    onChange && onChange(event);
+  };
+
+  return (
+    <div className={clsx(s.control, classContainer)}>
+      <div className={s.mainContainer}>
+        <label className={s.label}>
+          <input
+            {...rest}
+            disabled={!!disabled}
+            className={clsx(s.input, { [s.isError]: !!error }, className)}
+            onChange={changeHandler}
+          />
+        </label>
+      </div>
+      {error && <span className={s.error}>{error}</span>}
+    </div>
+  );
+};
+
+export default Input;
